9cf3392eb11d46bd7056e00e14d80b4c2d1d4e81
Pablo-Coronel <82612803+Pablo-Coronel@users.noreply.github.com>
2021-11-14 17:16:37 -0300

    Form contacto agregado

--- src/App.js
+++ src/App.js
@@ -1,49 +1,38 @@
-
-import './styles/App.css';
-import './components/Header';
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
-import Home from './pages/Home';
-import BecasPage from './pages/BecasPage';
-import Nosotros from './pages/Nosotros';
-import Carreras from './pages/Carreras/Carreras'
-
-
+import "./styles/App.css";
+import "./components/Header";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
+import FormContact from "./components/FormContact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BecasPage from "./pages/BecasPage";
+import Nosotros from "./pages/Nosotros";
+import Carreras from "./pages/Carreras/Carreras";
 
 function App() {
   return (
     <Router>
       <body>
-      <div className =" App">
-      <header>
-      <Header/>
-      </header>
-      
-      <Routes>
-          <Route exact path='/'  element={<Home/>} />
-          <Route exact path='/becas'  element={<BecasPage/>} />
-          <Route exact path='/nosotros' element={<Nosotros/>} />
-          <Route path='/carreras' element={Carreras} />
+        <div className=" App">
+          <header>
+            <Header />
+          </header>
 
-      </Routes>
-      
-      
-      
-      
-      <footer >
-        <Footer />
-      </footer>
-      </div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/becas" element={<BecasPage />} />
+            <Route exact path="/nosotros" element={<Nosotros />} />
+            <Route path="/carreras" element={Carreras} />
+          </Routes>
+
+          <footer>
+            <FormContact />
+            <Footer />
+          </footer>
+        </div>
       </body>
-      </Router>
-      
+    </Router>
   );
 }
 
