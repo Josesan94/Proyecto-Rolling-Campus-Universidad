64757004af2156601a572ed73018f725ed8fce34
Rocio Paula Mercado <82479829+romercado97@users.noreply.github.com>
2021-11-14 20:14:33 -0300

    carpeta backend

--- src/App.js
+++ src/App.js
@@ -37,3 +37,29 @@ function App() {
 }
 
 export default App;
+
+//BACKEND
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('Hola desde el servidor express ACTUALIZADO');
+})
+
+var user_routes = require('./routes/user');
+var transaction_routes = require('./routes/transaction');
+var upload_routes = require('./routes/upload')
+
+app.use(express.urlencoded({extended: true}));
+
+
+
+
+app.use('/api', [
+    user_routes, 
+    transaction_routes,
+    upload_routes
+])
+
+module.exports = app;
