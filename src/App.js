664518a201b4697ce53c2d737273d1d718eac1c0
Pablo-Coronel <82612803+Pablo-Coronel@users.noreply.github.com>
2021-11-14 15:40:37 -0300

    footer agregado

--- src/App.js
+++ src/App.js
@@ -35,22 +35,8 @@ function App() {
       </Routes>
       
       
-<<<<<<< HEAD
       
       
-=======
-      </header>
-      <div>
-        <h1>
-          
-        </h1>
-      </div>
-      <div>
-        <h1>
-          
-        </h1>
-      </div>
->>>>>>> 234517f6d8bfd9efb7f8331c923a0a44462a9f9e
       <footer >
         <Footer />
       </footer>
