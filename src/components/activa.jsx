289b9d69b805df85a3daae55067547967b2f184c
cristianzanesky11 <82786481+cristianzanesky11@users.noreply.github.com>
2021-11-23 13:19:26 -0300

    carrera

--- src/components/activa.jsx
+++ src/components/activa.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import estudiantes from '../assets/images/activa2.jpg'
 import {Button} from "react-bootstrap"
+import Image from 'react-bootstrap/Image'
 
 function Activa() {
     return (
         <div className="flexbox-container">
         <div className="activas"> 
-            <img src={estudiantes} alt="" />
+         <Image src={estudiantes} fluid />
         </div>
         <div className="activas-letras">
           <h1 className="activas-letras2"> Activa tu Educación</h1>
